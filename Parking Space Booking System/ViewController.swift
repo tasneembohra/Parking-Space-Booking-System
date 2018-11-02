f50a8f066b4111ee5620f1c4115c827b93aa6af7
Tasneem Bohra <tasneem.bohra@CA-TOR-WKST71PC.globalservs.com>
2018-11-02 19:29:17 -0400

    Splash Screen Design Done

--- Parking Space Booking System/ViewController.swift
+++ Parking Space Booking System/ViewController.swift
@@ -12,7 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(self.splashTimeOut(sender:)), userInfo: nil, repeats: false)
+    }
+    
+    @objc func splashTimeOut(sender : Timer) {
+        // TODO Check if the user is already loggedin then direct to Home instead Login
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: "loginVC")
+        
+        do {
+            try (UIApplication.shared.delegate as! AppDelegate).window?.rootViewController = vc
+        } catch {
+            
+        }
+       
     }
 
 
